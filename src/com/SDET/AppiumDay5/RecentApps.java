6d59bd36acbb3da8bb147fd6964bc2c2d1de01dd
George Scaria <sdygeorgescaria@gmail.com>
2021-02-16 11:37:01 +0530

    Navigation and NativeApp operations

---
+++ src/com/SDET/AppiumDay5/RecentApps.java
@@ -0,0 +1,53 @@
+package com.SDET.AppiumDay5;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class RecentApps {
+
+	public static void main(String[] args) throws Exception {
+
+DesiredCapabilities capabality = new DesiredCapabilities();
+		
+		//capabality.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+		capabality.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		capabality.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+		//capabality.setCapability("chromedriverExecutable", "C:\\Users\\GeorgeScaria\\git\\gSTAX-Appium\\chromedriver.exe");
+
+		capabality.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "org.khanacademy.android");
+		capabality.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "org.khanacademy.android.ui.library.MainActivity");
+		
+		AndroidDriver driver=new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),capabality);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Thread.sleep(10000);
+        
+        driver.pressKey(new KeyEvent(AndroidKey.HOME));
+        
+        driver.pressKey(new KeyEvent(AndroidKey.APP_SWITCH));
+        
+        driver.pressKey(new KeyEvent(AndroidKey.DPAD_DOWN));
+        
+        driver.pressKey(new KeyEvent(AndroidKey.DPAD_DOWN));
+        driver.pressKey(new KeyEvent(AndroidKey.DPAD_DOWN));
+        
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+        Thread.sleep(5000);
+        
+	}
+
+}
